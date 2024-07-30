0426ac162e91adde050e35fd3ad3fba8ff0f4691
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-07-30 21:42:36 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
-"""Module that determines if all boxes can be opened"""
-
 
 def canUnlockAll(boxes):
-    """
-    Determines if all boxes can be opened.
-    
-    Args:
-        boxes (list): A list of lists where each inner list represents a box.
-    
-    Returns:
-        bool: True if all boxes can be opened, else False.
-    """
     n = len(boxes)
-    unlocked = set([0])
-    keys = set(boxes[0])
-    
+    unlocked = [False] * n
+    unlocked[0] = True
+    keys = [0]
+
     while keys:
-        new_key = keys.pop()
-        if new_key not in unlocked:
-            if 0 <= new_key < n:
-                unlocked.add(new_key)
-                keys.update(set(boxes[new_key]) - unlocked)
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                keys.append(key)
     
-    return len(unlocked) == n
+    return all(unlocked)
 
-
+# Test cases
 if __name__ == "__main__":
     boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))
+    print(canUnlockAll(boxes))  # True
 
     boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))
+    print(canUnlockAll(boxes))  # True
 
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))
+    print(canUnlockAll(boxes))  # False
