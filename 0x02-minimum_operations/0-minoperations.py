77b321b81916824e363e80a67e9e49e9df16ea8a
ilyassdaaif <ilyass.df@gmail.com>
2024-08-06 15:05:59 -0700

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module for calculating minimum operations to achieve n H characters.
+"""
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to result in exactly
+    n H characters.
+
+    Args:
+    n (int): The target number of H characters.
+
+    Returns:
+    int: The minimum number of operations needed,
+    or 0 if n is impossible to achieve.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
