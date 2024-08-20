9f877f41000377240f0b914f8f2cdf780aa50920
ilyassdf <ilyassdaaif@gmail.com>
2024-08-20 09:54:19 -0700

    a

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -8,7 +8,10 @@ validUTF8 = __import__('0-validate_utf8').validUTF8
 data = [65]
 print(validUTF8(data))
 
-data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+data = [80, 121, 114, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+print(validUTF8(data))
+
+data = [229, 65, 127, 256]
 print(validUTF8(data))
 
 data = [65, 127, 128, 129, 240, 159, 130, 140]
