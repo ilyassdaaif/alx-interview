5fc317cbd9b733720080214c402254313abbbcf1
ilyassdf <ilyassdaaif@gmail.com>
2024-08-20 09:49:36 -0700

    a

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -11,5 +11,5 @@ print(validUTF8(data))
 data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
 print(validUTF8(data))
 
-data = [229, 65, 127, 256]
+data = [65, 127, 128, 129, 240, 159, 130, 140]
 print(validUTF8(data))
