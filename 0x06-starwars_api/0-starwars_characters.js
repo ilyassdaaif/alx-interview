39b5a7639a54cbd54598cd10d541eb28f6127482
ilyassdaaif <ilyass.daaif@gmail.com>
2024-09-04 14:45:12 -0700

    a

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,38 +1,35 @@
 #!/usr/bin/node
-// Prints all characters of a Star Wars movie.
 
 const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-// Fetch movie data first
-request(url, (error, response, body) => {
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+function fetchCharacter(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+}
+
+request(apiUrl, async (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.error('Error:', error);
     return;
   }
 
-  const film = JSON.parse(body);
-  const characters = film.characters;
+  const movie = JSON.parse(body);
+  const characterUrls = movie.characters;
 
-  // Create an array of promises for each character request
-  const characterPromises = characters.map(characterUrl => {
-    return new Promise((resolve, reject) => {
-      request(characterUrl, (charError, charResponse, charBody) => {
-        if (charError) {
-	  reject(charError);
-	} else {
-	  const character = JSON.parse(charBody);
-	  resolve(character.name);
-        }
-      });
-    });
-  });
-
-  // Wait for all character requests to complete
-  Promise.all(characterPromises)
-    .then(characterNames => {
-      characterNames.forEach(name => console.log(name));
-    })
-    .catch(err => console.error(err));
+  try {
+    const characterNames = await Promise.all(characterUrls.map(fetchCharacter));
+    characterNames.forEach(name => console.log(name));
+  } catch (err) {
+    console.error('Error fetching characters:', err);
+  }
 });
