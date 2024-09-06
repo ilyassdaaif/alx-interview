58b40c4b9ecbf4a4d8374b7b8bd1e9eb9352f65c
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-09-06 12:40:50 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,35 +5,41 @@ Module for the canUnlockAll function.
 
 
 def canUnlockAll(boxes):
-    """
-    Determines if all boxes can be opened.
-
-    Args:
-    boxes (list of lists): A list of lists where each inner list represents a box
-                           and contains keys to other boxes.
-
-    Returns:
-    bool: True if all boxes can be opened, else False.
-    """
+    # Number of boxes
     n = len(boxes)
-    unlocked = set([0])  # Start with box 0 unlocked
-    keys = set(boxes[0])  # Keys from the first box
+    
+    # Set to track visited boxes
+    visited = set()
+    
+    # Queue for BFS, start with box 0
+    queue = [0]
+    
+    while queue:
+        # Get the current box
+        current_box = queue.pop(0)
+        
+        # If this box has already been visited, continue
+        if current_box in visited:
+            continue
+        
+        # Mark the current box as visited
+        visited.add(current_box)
+        
+        # Add all keys in the current box to the queue
+        for key in boxes[current_box]:
+            if key not in visited:
+                queue.append(key)
+    
+    # Check if we have visited all boxes
+    return len(visited) == n
 
-    while keys:
-        new_key = keys.pop()
-        if new_key < n and new_key not in unlocked:
-            unlocked.add(new_key)
-            keys.update(boxes[new_key])
-
-    return len(unlocked) == n
-
-
+# Test cases
 if __name__ == "__main__":
     boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))
+    print(canUnlockAll(boxes))  # True
 
     boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))
+    print(canUnlockAll(boxes))  # True
 
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))
+    print(canUnlockAll(boxes))  # False
