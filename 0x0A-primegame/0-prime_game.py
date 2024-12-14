dbd5693ec02037259c46c628c634021349618154
ilyassdf <ilyass.df.01@gmail.com>
2024-12-14 14:37:11 -0800

    0x0A primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,51 +1,42 @@
 #!/usr/bin/python3
-"""
-Prime Game Module
-"""
+"""0. Prime Game - Maria and Ben are playing a game"""
 
 
 def isWinner(x, nums):
+    """x - rounds
+    nums - numbers list
     """
-    Determines winner of Prime Game
-    Args:
-        x (int): number of rounds
-        nums (list): array of n
-    Returns:
-        str: name of winner (Maria or Ben) or None
-        if winner cannot be determined
-    """
-    if not nums or x < 1:
+    if x <= 0 or nums is None:
         return None
-    max_num = max(nums)
+    if x != len(nums):
+        return None
 
-    # Generate primes up to max number using Sieve of Eratosthenes
-    primes = [True for _ in range(max(max_num + 1, 2))]
-    primes[0] = primes[1] = False
+    ben = 0
+    maria = 0
 
-    for i in range(2, int(pow(max_num, 0.5)) + 1):
-        if primes[i]:
-            for j in range(i * i, max_num + 1, i):
-                primes[j] = False
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
 
-    # Count primes up to each number in nums
-    prime_counts = [0] * (max_num + 1)
-    for i in range(2, max_num + 1):
-        prime_counts[i] = prime_counts[i - 1]
-        if primes[i]:
-            prime_counts[i] += 1
-
-    maria_wins = 0
-    ben_wins = 0  # Initialize ben_wins
-    for n in nums:
-        if n < 2:
-            ben_wins += 1
-        elif prime_counts[n] % 2 == 1:
-            maria_wins += 1
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
         else:
-            ben_wins += 1
-
-    if maria_wins * 2 > x:
-        return "Maria"
-    elif maria_wins * 2 < x:
+            maria += 1
+    if ben > maria:
         return "Ben"
+    if maria > ben:
+        return "Maria"
     return None
+
+
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
