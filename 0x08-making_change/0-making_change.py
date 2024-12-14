878babb7b2488b3c8ef0ba42f4a34185238d098e
ilyassdf <94968783+ilyassdaaif@users.noreply.github.com>
2024-12-15 00:59:09 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
-"""
-Determine the fewest number of coins needed to meet a given amount total
-"""
-total1 = 37
-list_of_coins1 = [1, 2, 25]
 
-#total2 = 1453
-#list_of_coins2 = [1256, 54, 48, 16, 102]
+""" Contains makeChange function"""
+
 
 def makeChange(coins, total):
+    """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-
-    remainder = total  # 37
-
-    coins_needed = 0
-
-    coin_index = 0
-
-    sorted_coins_list = sorted(coins, reverse=True)
-    # [25, 2, 1]
-
-    list_len = len(coins)  # 3
-
-    while remainder > 0:  # 0
-
-        if coin_index >= list_len:  # index 1 < 3
-            return -1
-
-        if remainder - sorted_coins_list[coin_index] >= 0:  # 2 - 2 = 0
-            remainder -= sorted_coins_list[coin_index]  # 0
-
-            coins_needed += 1  # 1
-        else:
-            coin_index += 1
-    return coins_needed  # 7
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
