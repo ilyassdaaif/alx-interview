f27866187f4fc333f1dd9e02d193410a13505fac
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-12-14 23:49:44 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
-'''LockBoxes Challenge'''
-
+"""
+Module to determine if all boxes can be unlocked
+"""
 
 def canUnlockAll(boxes):
-    '''determines if all the boxes can be opened or not
+    """
+    Determines if all boxes can be opened.
+    
+    Args:
+        boxes (list of lists): A list where each index represents a box,
+                                and the value is a list of keys to other boxes.
+    
     Returns:
-        True: all boxes can be opened
-        False: not all boxes can be opened
-    '''
-    length = len(boxes)
-    keys = set()
-    opened_boxes = []
-    i = 0
-
-    while i < length:
-        oldi = i
-        opened_boxes.append(i)
-        keys.update(boxes[i])
-        for key in keys:
-            if key != 0 and key < length and key not in opened_boxes:
-                i = key
-                break
-        if oldi != i:
-            continue
-        else:
-            break
-
-    for i in range(length):
-        if i not in opened_boxes and i != 0:
-            return False
-    return True
+        bool: True if all boxes can be opened, False otherwise
+    """
+    if not boxes:
+        return False
+    
+    n = len(boxes)
+    
+    opened = set([0])  
+    
+    keys_to_process = list(boxes[0])
+    
+    while keys_to_process:
+        key = keys_to_process.pop(0)
+        
+        if 0 <= key < n and key not in opened:
+            opened.add(key)
+            
+            keys_to_process.extend(boxes[key])
+    
+    return len(opened) == n
